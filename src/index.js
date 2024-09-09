6556be47dd5d7839c87038efb5ddecaaa02fe013
aryanserasinghe <aryanserasinghe1234@gmail.com>
2024-09-09 19:50:14 +0530

    Resolved conflicts and updated files

--- src/index.js
+++ src/index.js
@@ -17,12 +17,19 @@ app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
+// Render Main Page
 app.get("/", (req, res) => {
     res.render("login");
 });
 
+// Render Registration Page
 app.get("/registration", (req, res) => {
     res.render("registration");
+});
+
+// Render Login Page
+app.get("/login", (req, res) => {
+    res.render("login");
 });
 
 //Register User
@@ -39,11 +46,34 @@ app.post("/registration",async (req, res) => {
     if(existingUser) {
         res.send("User already exist.");
     }else {
-        //hash the password
+        //hash the password using bycrypt
+        const saltRounds = 10; //Number of salt round for bycrypt
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+        //Relplacing the hash password with original password
+        data.password = hashedPassword; 
     const userdata = await collection.insertMany(data);
     console.log(userdata);
     }
 });
+
+//Login User
+
+app. post("/login", async (req, res) => {
+    try{
+        const check = await collection.findOne({name: req.body.username});
+        if(!check) {
+            res.send("User Cannot Be Found");
+        }
+
+        //Compare the hash password from the database with the plain text
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if(isPasswordMatch) {
+            res.render("attendance");
+        }
+    }catch{
+        res.send("Wrong Details");
+    }
+});    
 
 const port = 5000;
 app.listen(port, () => {
